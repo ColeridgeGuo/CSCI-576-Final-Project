3a56c22021d467fbf9c92ba254a46d135b93baf7
ColeridgeGuo <yingxuan.guo9771@gmail.com>
2020-11-27 22:03:16 -0800

    separate file reading and video playing

--- MediaQuery.py
+++ MediaQuery.py
@@ -32,16 +32,23 @@ def RGB_to_YUV(rgb: np.ndarray) -> np.ndarray:
 class MediaQuery:
     
     def __init__(self, fpath: str):
+        self.root = tk.Tk()  # base GUI container
+        self.label = tk.Label(self.root)  # tkinter label to show image
+        
         vid_name = fpath.split('/')[-1]  # extract video name from file path
         self.vid_name = vid_name
         self.fp = fpath
-        
-        self.root = tk.Tk()  # base GUI container
-        self.frames = None  # list of PhotoImages to display
-        # When you add a PhotoImage or other Image object to a Tkinter widget,
-        # you must keep your own reference to the image object.
-        self.data = None
-        self.label = tk.Label(self.root)  # tkinter label to show image
+        # file paths to all frames
+        dirs = sorted(os.listdir(self.fp), key=lambda x: int(x[5:-4]))
+        fpaths = [f'{self.fp}/{frame}' for frame in dirs[:VID_LEN * FPS]]
+        # read in all frames' rgb values
+        print(f'Reading RGB values of video "{self.vid_name}"...')
+        self.data = np.array([read_image_RGB(fpath)
+                              for fpath in progressbar(fpaths)])
+        # convert rgb values to PhotoImage for display in GUI
+        self.frames = [PhotoImage(Image.fromarray(d)) for d in self.data]
+        # When you add a PhotoImage to a Tkinter widget, you must keep your own
+        # reference to the image object, hence storing them in a list
         
     def calc_motion(self) -> int:
         # recast datatype to avoid over/underflow
@@ -101,18 +108,6 @@ class MediaQuery:
         self.root.after(40, self.update, ind)  # call to display next frame
         
     def show_video(self) -> NoReturn:
-        
-        # file paths to all frames
-        dirs = sorted(os.listdir(self.fp), key=lambda x: int(x[5:-4]))
-        fpaths = [f'{self.fp}/{frame}' for frame in dirs[:VID_LEN * FPS]]
-        
-        # read in all frames' rgb values
-        print(f'Reading RGB values of video "{self.vid_name}"...')
-        self.data = np.array([read_image_RGB(fpath)
-                              for fpath in progressbar(fpaths)])
-        # convert rgb values to PhotoImage for display in GUI
-        self.frames = [PhotoImage(Image.fromarray(d)) for d in self.data]
-        
         self.label.pack()
         # callback update() to automatically update frames
         self.root.after(0, self.update, 0)
@@ -124,6 +119,6 @@ if __name__ == '__main__':
     file_path = args[1]
     vd = MediaQuery(file_path)
     vd.show_video()
-
+    
     motion_in_vid = vd.calc_motion()
     print(f'The motion in video "{vd.vid_name}": {motion_in_vid}')
